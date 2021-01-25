e570bf3cfa2f866268eef57a16afd045e264d5ca
Administrator <519923938@qq.com>
2021-01-25 09:59:13 +0800

    我实现的23种设计模式——持续更新

---
+++ src/main/java/单例设计模式/双重校验锁/DoubleLockSingleton.java
@@ -0,0 +1,17 @@
+package 单例设计模式.双重校验锁;
+//懒汉式双重校验锁
+public class DoubleLockSingleton {
+    private static volatile DoubleLockSingleton doubleLockSingleton;
+    private DoubleLockSingleton(){}
+    public static DoubleLockSingleton getInstance(){
+        if(doubleLockSingleton==null){
+            synchronized (DoubleLockSingleton.class){
+                if(doubleLockSingleton==null){
+                    doubleLockSingleton = new DoubleLockSingleton();
+                    return doubleLockSingleton;
+                }
+            }
+        }
+        return doubleLockSingleton;
+    }
+}
